e8f4fc930bde73856a922cdfe8546fba6129d9e9
prozella <kiwulik@yandex.ru>
2023-05-27 11:07:09 +0700

    NSU-41 добавил логирование

--- src/main/java/org/example/application/fabric/JiraConfig.java
+++ src/main/java/org/example/application/fabric/JiraConfig.java
@@ -1,9 +1,12 @@
 package org.example.application.fabric;
 
+import lombok.extern.log4j.Log4j2;
+
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
 
+@Log4j2
 public class JiraConfig {
 
     public static String LOGIN = null;
@@ -24,5 +27,11 @@ public class JiraConfig {
         SEARCH_JQL = properties.getProperty("search_jql");
         TRANSITION_TEMPLATE = properties.getProperty("transition_template");
         DESCRIPTION_TEMPLATE = properties.getProperty("description_template");
+        if (null == LOGIN || LOGIN.isEmpty()){
+            log.warn("Jira login is not provided.");
+        }
+        if (null == API_TOKEN || API_TOKEN.isEmpty()){
+            log.warn("Jira api token is not provided.");
+        }
     }
 }
